c65f91b707ea54bbc3c1ae028a91a46c9b7c29c3
gvijaynayak <vijayg2409@gmail.com>
2026-06-09 15:35:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ if st.button("Predict MSRP"):
 
     try:
         # Load original dataset used during training
-        train_df = pd.read_csv("car_MSRP.csv")
+        train_df = pd.read_csv("car_MSRP")
 
         target_col = "MSRP (Manufacturer's suggested retail Price)"
 
